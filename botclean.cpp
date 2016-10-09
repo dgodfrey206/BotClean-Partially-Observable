4c0ecc943f39908a7c327d6b6b12992c8b393a5d
David Godfrey <david.godfrey99@gmail.com>
2016-10-09 11:39:44 -0700

    Update botclean.cpp

--- botclean.cpp
+++ botclean.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
+#include<climits>
 #include<cstdlib>
 using namespace std;
 
 struct node {
   int x, y;
   node(int x,int y):x(x),y(y){}
-  int distTo(node n) const {
+  bool operator==(node rhs) const {return(x==rhs.x)&&(y==rhs.y);}
+  bool operator!=(node rhs)const{return!(*this==rhs);  }
+  int delta(node n) const {
     return abs(x - n.x) + abs(y - n.y);
   }
 };
 
-node null = {-1,-1};
-
-bool operator==(node lhs,node rhs){return (lhs.x==rhs.x)&&(lhs.y==rhs.y);}
-bool operator!=(node lhs,node rhs){return!(lhs==rhs);}
+node null = {INT_MAX,INT_MAX};
 
 string print_move(int x, int y) {
   if (x < 0)
@@ -43,30 +43,28 @@ void next_move(int posr, int posc, vector<string> board) {
   node nearestCell = null;
   for (i = 0; i < dirty_cells.size(); ++i) {
     node n = dirty_cells[i];
-    if (nearestCell == null || bot.distTo(n) < bot.distTo(nearestCell))
+    if (nearestCell == null || bot.delta(n) < bot.delta(nearestCell))
       nearestCell = n;
   }
+  // if we found a dirty cell go in its direction
   if (nearestCell != null)
-    cout << print_move(bot.x - nearestCell.x, bot.y - nearestCell.y) << '\n';
+    cout << print_move(bot.x - nearestCell.x, bot.y - nearestCell.y);
   else {
     // otherwise move in a clockwise motion...
-    string leftMove = print_move(0,1), rightMove = print_move(0,-1),
-           upMove   = print_move(1,0), downMove = print_move(-1,0);
     node topLeft    = node(bot.x-1,bot.y-1), topRight = node(bot.x-1,bot.y+1),
          bottomLeft = node(bot.x+1,bot.y-1), bottomRight = node(bot.x+1,bot.y+1);
     
+    // check corners
     if (topRight.y >= m-1 && bottomRight.x < n-1)
-      cout << downMove; // move down if at the right corner
+      cout << "DOWN"; // move down if at the right corner
     else if (bottomRight.x >= n-1 && bottomLeft.y > 0)
-      cout << leftMove; // move left if at bottom corner
+      cout << "LEFT"; // move left if at bottom corner
     else if (bottomLeft.y <= 0 && topLeft.x > 0)
-      cout << upMove; // move up if at the left corner
+      cout << "UP"; // move up if at the left corner
     else if (topLeft.x <= 0 && topRight.y < m-1)
-      cout << rightMove; // move right if at the top corner
+      cout << "RIGHT"; // move right if at the top corner
     else // it's somewhere between the corners, just move right
-    {
-      cout << rightMove;
-    }
+      cout << "RIGHT";
   }
 }
 
@@ -74,7 +72,7 @@ int main() {
     int pos[2];
     vector<string> board;
     cin >> pos[0] >> pos[1];
-    for (int i = 0; i < 5; i++) {
+    for(int i = 0; i < 5; i++) {
         string s; cin >> s;
         board.push_back(s);
     }
